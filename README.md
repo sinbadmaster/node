7719a9ac17736263829c129ea6a5c138bd6f8f9f
sinbadmaster <bless_lee@126.com>
2020-06-17 14:52:41 +0800

    modified: readme完善，文件路径完善

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# node
-学习node的库
+# json2Excel
+
+```npm install json2excel-node --save```
+
+将对象或数组对象转为excel, 暂时只支持了数组嵌套对象，不支持多重嵌套
+
+1. 安装后的使用方式：
+```javascript
+const json2Excel = require('json2excel-node');
+json2Excel(savePath, fileName, jsonData, options);
+```
+
+2. 参数说明：
+
+|参数名|参数类型|是否必填|描述
+|:--|:--|:--|:--
+|savePath|string|Y|转换为excel后的保存路径
+|fileName|string|Y|转换为excel后的文件名
+|jsonData|Object,Array|Y|待转换的数据
+|options|Object|N|选项参数，详情见下方表格
+
+options参数说明：
+|参数名|参数类型|是否必填|描述
+|:--|:--|:--|:--
+|headers|Object|N|表头对象，key值为列对应数据，value为表头内容
+
+
+如传入如下headers，jsonData中的**name**字段将填充到**姓名**列。
+```javascript
+headers: { name: '姓名' }
+```
--- index.js
+++ index.js
@@ -2,27 +2,34 @@
  * @Author: sinbadMaster
  * @Description: 将json转为excel
  * @Date: 2020-06-09 08:59:11
- * @LastEditTime: 2020-06-17 10:02:31
+ * @LastEditTime: 2020-06-17 14:29:41
  * @FilePath: \json2Excel\index.js
  */ 
 
 const Excel = require('exceljs');
 const path = require('path');
+const fs = require('fs');
 
 /**
  * @description: 将对象或数组对象转为excel, 暂时只支持了数组嵌套对象，不支持多重嵌套
  * @param {string} savePath 保存路径
  * @param {string} fileName 文件名 
  * @param {Object} jsonData json格式的数据
+ * @param {{headers: {[string]: string}}} options 选项参数可选，如显示的传入headers则以此为表头，
+ * 如未传入则以数组第一项对象的key值作为表头
  * @return: 
  * @Date: 2020-06-09 15:13:30
  */
-exports.default = function json2Excel(savePath, fileName, jsonData) {
+module.exports = function json2Excel(savePath, fileName, jsonData, options) {
   savePath = savePath.endsWith(path.sep) ? savePath : savePath + path.sep;
   fileName = (fileName.endsWith('.xls') || fileName.endsWith('.xlsx'))
     ? fileName
     : fileName + '.xlsx'
 
+  if (!fs.existsSync(savePath)) {
+    fs.mkdirSync(savePath, { recursive: true })
+  }
+  
   const workbook = new Excel.stream.xlsx.WorkbookWriter({
     filename: `${savePath}${fileName}`
   });
@@ -30,13 +37,18 @@ exports.default = function json2Excel(savePath, fileName, jsonData) {
   const worksheet = workbook.addWorksheet('sheet');
 
   const isArray = Array.isArray(jsonData);
-  const header = isArray
-    ? jsonData.length
-        ? jsonData[0]
-        : {}
-    : jsonData;
   
-  worksheet.columns = Object.keys(header).map(prop => ({ header: prop, key: prop }));
+  if (typeof options === 'object' && typeof options.headers === 'object') {
+    worksheet.columns = Object.entries(options.headers).map(item => ({header: item[1], key: item[0]}));
+  } else {
+    const header = isArray
+      ? jsonData.length
+          ? jsonData[0]
+          : {}
+      : jsonData;
+    
+    worksheet.columns = Object.keys(header).map(prop => ({ header: prop, key: prop }));
+  }  
 
   if (isArray) {
     for (const row of jsonData) {
